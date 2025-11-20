b4cdaad6b776be460b3c9312100538f7e20cc06f
candoll0430tsuyoshi-afk <candoll.0430.tsuyoshi@gmail.com>
2025-11-20 16:52:06 +0900

    Update freeTimes.js

--- api/freeTimes.js
+++ api/freeTimes.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   });
 
   const calendar = google.calendar({ version: "v3", auth });
-  const calendarId = "candoll-reserve@candoll.iam.gserviceaccount.com";
+  const calendarId = "candoll202601@gmail.com";
 
   // 日付の開始と終了（UTCに注意）
   const start = new Date(`${date}T10:00:00`);
