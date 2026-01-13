538b1620f224119e866aecddbeb942f906313661
candoll0430tsuyoshi-afk <candoll.0430.tsuyoshi@gmail.com>
2026-01-13 12:45:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function setupSelectColorChange(selectElement) {
 }
 
 function renderMenuOptions(selectElement, data, categories) {
-  selectElement.innerHTML = '<option value="">メニューを選択してください</option>';
+  selectElement.innerHTML = '<option value="">メニューを選択</option>';
   
   Object.keys(categories).forEach(catName => {
     const group = document.createElement("optgroup");
